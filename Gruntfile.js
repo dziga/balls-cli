fb2c9d9ca5dbbbe7f4dcf36705df398fdcca6702
Nikola Milivojevic <0dziga0@gmail.com>
2015-11-26 23:33:47 +0100

    copy sounds to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -329,7 +329,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'sounds/{,*/}*.*'
           ]
         }, {
           expand: true,
